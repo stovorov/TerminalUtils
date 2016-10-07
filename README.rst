102855c6ab29697699954a2b48fb6c5548be2fcb
Pawel <pawel@localhost.localdomain>
2016-10-07 13:35:47 +0200

    minor changes

--- README.rst
+++ README.rst
@@ -9,6 +9,8 @@ These includes:
 1. progress bar
 2. colored printing 
 3. quick redirection of std_out to file
-4. simple logging function for profiling functions calls
+4. simple logging for profiling functions calls
+
+Tested for Python 2.6 and 2.7. Python 3.x is currently unsupported.
 
 .. |Build-Status| image:: https://travis-ci.org/stovorov/TerminalUtils.svg?branch=master
--- TerminalUtils/__init__.py
+++ TerminalUtils/__init__.py
@@ -3,9 +3,9 @@ import sys
 if sys.version_info >= (3,):
 	raise NotImplemented('Library not ready for Python 3.x')
 else:
-	from _terminalutils import ProgressBarPy2x as ProgressBar
-	from _terminalutils import GetFunctionStatsPy2x as GetFunctionStats
-	from _terminalutils import c_print_py2x as c_print
-	from _terminalutils import std_out2file_py2x as std_out2file
+	from _terminalutils2x import ProgressBarPy2x as ProgressBar
+	from _terminalutils2x import GetFunctionStatsPy2x as GetFunctionStats
+	from _terminalutils2x import c_print_py2x as c_print
+	from _terminalutils2x import std_out2file_py2x as std_out2file
 
 __all__ = ['ProgressBar', 'GetFunctionStats', 'c_print', 'std_out2file']
